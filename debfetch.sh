af1db25e7d544d21151b6f45a9d784f531dcfca9
Leandro Cunha <leandrocunha016@gmail.com>
2021-09-05 02:37:00 -0300

    first commit

---
+++ debfetch.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Include 'os-release' to grub Debian prettt name
+
+. /etc/os-release
+
+# Styles
+
+bold="$(tput bold)"
+reset="$(tput sgr0)"
+red="$(tput setaf 1)"
+
+# Get terminal col width
+
+cols=$(tput cols)
+
+clear
+
+echo "
+$red ⢀⣴⠾⠻⢶⣦⠀  $reset ${red}User: $USER${reset}
+$red ⣾⠁⢠⠒⠀⣿⡁  $reset ${red}OS:${bold}${reset} $PRETTY_NAME
+$red ⢿⡄⠘⠷⠚⠋⠀  $reset ${red}Kernel:${bold}${reset} $(uname -rm)
+$red ⠈⠳⣄⠀⠀⠀⠀  $reset ${red}Uptime:${bold}${reset} $(uptime -p | cut -d' ' -f2-)
+"
+
+printf -- "-%.s" $(seq $cols)
+
+echo "\n"
+
+exit 0
